bfe170711588c2b98ed1ca1df023c7e4c4183072
WizarDiger <WizarDigeR@gmail.com>
2023-11-19 20:42:53 +0300

    Ez solution

--- Contest/TheMillionthFibonacciKata/WizarDiger/Fibonacci.cs
+++ Contest/TheMillionthFibonacciKata/WizarDiger/Fibonacci.cs
@@ -4,8 +4,17 @@ namespace TheMillionthFibonacciKata.WizarDiger;
 
 public class Fibonacci
 {
-    public static BigInteger Fib(int n)
+    public static BigInteger fib(int n)
     {
-        return BigInteger.Zero;
+        var myArr = new int[n];
+        myArr[0] = 0;
+        myArr[1] = 1;
+        if (n == 0) return myArr[0];
+        if (n == 1) return myArr[1];
+            for (int i = 2; i < n; i++)
+            {
+                myArr[i] = myArr[i - 1] + myArr[i - 2];
+            }
+        return myArr[n - 1];
     }
 }
