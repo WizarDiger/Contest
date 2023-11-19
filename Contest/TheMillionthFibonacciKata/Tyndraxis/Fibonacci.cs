0d0fb792db3742b655f4a86849222ada52fed881
dykemlr1 <dykemlr1@gmail.com>
2023-11-19 22:39:01 +0300

    Upgrade

--- Contest/TheMillionthFibonacciKata/Tyndraxis/Fibonacci.cs
+++ Contest/TheMillionthFibonacciKata/Tyndraxis/Fibonacci.cs
@@ -8,13 +8,13 @@ public class Fibonacci
     private static readonly double secondFi = (1 - Math.Sqrt(5)) / 2;
 
     public static BigInteger Fib(int n)
-    {
+    { 
         var modifier = n >= 0
             ? 1L
             : n % 2 > 0
                 ? -1L
                 : 1;
-        return new BigInteger(modifier * (long) Math.Round(SolveFibonacci(n)));
+        return new BigInteger(modifier * Math.Round(SolveFibonacci(n)));
     }
 
     private static double SolveFibonacci(int n)
