3579994ccc6482d050717eef49a15a9c54bcdb01
dykemlr1 <dykemlr1@gmail.com>
2023-11-19 20:08:55 +0300

    Fibonacci

---
+++ Contest/TheMillionthFibonacciKata/Fibonacci.cs
@@ -0,0 +1,11 @@
+using System.Numerics;
+
+namespace TheMillionthFibonacciKata;
+
+public class Fibonacci
+{
+    public static BigInteger Fib(int n)
+    {
+        return BigInteger.Zero;
+    }
+}
