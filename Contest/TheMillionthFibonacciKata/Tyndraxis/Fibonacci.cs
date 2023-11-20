f72e0476b5f7c0cd6be4ee1267200b8c20eebcbd
dykemlr1 <dykemlr1@gmail.com>
2023-11-20 21:16:45 +0300

    Table method fibonacci solution

--- Contest/TheMillionthFibonacciKata/Tyndraxis/Fibonacci.cs
+++ Contest/TheMillionthFibonacciKata/Tyndraxis/Fibonacci.cs
@@ -4,20 +4,40 @@ namespace TheMillionthFibonacciKata.Tyndraxis;
 
 public class Fibonacci
 {
-    private static readonly double firstFi = (1 + Math.Sqrt(5)) / 2;
-    private static readonly double secondFi = (1 - Math.Sqrt(5)) / 2;
+    private static readonly BigInteger[,] matrix = new BigInteger[,]
+    {
+        {0, 1},
+        {1, 1}
+    };
+
+    private static readonly BigInteger[,] identityMatrix = new BigInteger[,]
+    {
+        {1, 0},
+        {0, 1}
+    };
 
     public static BigInteger Fib(int n)
     {
-        var modifier = n >= 0
-            ? 1
-            : n % 2 > 0
-                ? -1
-                : 1;
-        var result = modifier * Math.Round(SolveFibonacci(n));
-        return new BigInteger(result);
+        var nAbs = Math.Abs(n);
+        var result = identityMatrix;
+        var bits = Convert.ToString(nAbs, 2);
+        foreach (var bit in bits)
+        {
+            result = Multiply(result, result);
+            if (bit == '1')
+            {
+                result = Multiply(result, matrix);
+            }
+        }
+
+        var modifier = n >= 0 ? 1 : Math.Pow(-1, n + 1);
+        return new BigInteger(modifier) * result[1, 0];
     }
 
-    private static double SolveFibonacci(int n)
-        => (Math.Pow(firstFi, n) - Math.Pow(secondFi, n)) / Math.Sqrt(5);
+    private static BigInteger[,] Multiply(BigInteger[,] matrix1, BigInteger[,] matrix2)
+        => new[,]
+        {
+            {matrix1[0, 0] * matrix2[0, 0] + matrix1[0, 1] * matrix2[1, 0], matrix1[0, 0] * matrix2[0, 1] + matrix1[0, 1] * matrix2[1, 1]},
+            {matrix1[1, 0] * matrix2[0, 0] + matrix1[1, 1] * matrix2[1, 0], matrix1[1, 0] * matrix2[0, 1] + matrix1[1, 1] * matrix2[1, 1]}
+        };
 }
